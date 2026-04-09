82bcd4e6366a07ef62ea524d750fecca5b77e45e
Yuva Bhatta <77378462+yuvarajbhatta@users.noreply.github.com>
2026-04-08 22:44:58 -0500

    Layout fixed (#3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         {children}
         <ScrollToTopButton />
       </body>
